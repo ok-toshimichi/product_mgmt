0faff99e900026d42fc3c4e9eae8500e2d8a3895
toshimichi <tea_lfilni_m_une_l_more_h@yahoo.co.jp>
2022-12-12 11:27:03 +0900

    product_nameをwordで設定

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'company_id' => $faker->numberBetween($min = 1, $max = 5),
-        'product_name' => $faker->unique()->lexify('item????'),
+        'product_name' => $faker->unique()->word,
         'price' => $faker->numberBetween($min = 100, $max = 2000),
         'stock' => $faker->numberBetween($min = 10, $max = 300),
         'comment' => $faker->realText,
