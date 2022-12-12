29859d658efe2a381794252e3a7c0ff6641a049d
toshimichi <tea_lfilni_m_une_l_more_h@yahoo.co.jp>
2022-12-12 11:12:08 +0900

    factory作成

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'company_id' => $faker->numberBetween($min = 1, $max = 5),
+        'product_name' => $faker->unique()->lexify('item????'),
+        'price' => $faker->numberBetween($min = 100, $max = 2000),
+        'stock' => $faker->numberBetween($min = 10, $max = 300),
+        'comment' => $faker->realText,
+    ];
+});
